8b96426e4a5be4ee93da2bb71a043a63de6d4eab
AltayMahmudi <AltaySHm@code.edu.az>
2020-05-16 18:28:00 +0400

    Jotex First

---
+++ JotexRepository/Data/JotexDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace JotexRepository.Data
+{
+    public class JotexDbContext:DbContext
+	{
+		public JotexDbContext(DbContextOptions<JotexDbContext> options) { }
+	}
+}
